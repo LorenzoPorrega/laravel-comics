f3f3d4af85a710c0f9ca94b12d84064f64c184b1
LorenzoPorrega <lorenzoporrega@hotmail.it>
2023-09-27 15:55:56 +0200

    final push - comple exercise

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,57 +3,65 @@
     [
       "title" => "Digital Comics",
       "sub" => [
-        "title" => "Character",
-        "title" => "Comics",
-        "title" => "Movies",
-        "title" => "TV",
-        "title" => "Games",
-        "title" => "News"
+        "1" => "Character",
+        "2" => "Comics",
+        "3" => "Movies",
+        "4" => "TV",
+        "5" => "Games",
+        "6" => "News"
       ],
     ],
     [
       "title" => "Shop",
       "sub" => [
-        "title" => "Shop DC",
-        "title" => "Shop DC Collectibles"
+        "1" => "Shop DC",
+        "2" => "Shop DC Collectibles"
       ],
     ],
     [
-      "title" => "Digital Comics",
+      "title" => "DC",
       "sub" => [
-        "title" => "Terms Of Use",
-        "title" => "Privacy policy (New)",
-        "title" => "Ad Choices",
-        "title" => "Advertising",
-        "title" => "Jobs",
-        "title" => "Subscriptions",
-        "title" => "Talent Workshops",
-        "title" => "Talent Workshops",
-        "title" => "CPSC Certificates",
-        "title" => "Ratings",
-        "title" => "Shop Help",
-        "title" => "Contact Us"
+        "1" => "Terms Of Use",
+        "2" => "Privacy policy (New)",
+        "3" => "Ad Choices",
+        "4" => "Advertising",
+        "5" => "Jobs",
+        "6" => "Subscriptions",
+        "7" => "Talent Workshops",
+        "8" => "Talent Workshops",
+        "9" => "CPSC Certificates",
+        "10" => "Ratings",
+        "11" => "Shop Help",
+        "12" => "Contact Us"
       ],
     ],
     [
       "title" => "Sites",
       "sub" => [
-        "title" => "DC",
-        "title" => "MAD Magazine",
-        "title" => "DC Kids",
-        "title" => "DC Universe",
-        "title" => "DC Power Visa"
+        "1" => "DC",
+        "2" => "MAD Magazine",
+        "3" => "DC Kids",
+        "4" => "DC Universe",
+        "5" => "DC Power Visa"
       ],
     ]
-];
+  ];
+
+  $socialIcons = [
+    "facebook" => "./../img/footer-facebook.png",
+    "x" => "./../img/footer-twitter.png",
+    "youtube" => "./../img/footer-youtube.png",
+    "pinterest" => "./../img/footer-pinterest.png",
+    "periscope" => "./../img/footer-periscope.png"
+  ]
 @endphp
 
 <footer>
   <div class="top-footer overflow-hidden">
-    <div class="container h-100 py-5">
+    <div class="container h-100 pt-5 pb-1">
       <div class="row">
         <div class="col-6">
-          <div class="row">
+          <div class="row g-1">
             @foreach ($menuItems as $menuTitle)
               <div class="col-3">
                 <div class="title-box">
@@ -61,18 +69,39 @@
                 </div>
                 <ul class="list-unstyled">
                   @foreach ($menuTitle['sub'] as $singleSubTitle)
-                    <li class="sub-title">{{ $singleSubTitle }}</li>
+                    <li><a href="" class="sub-title">{{ $singleSubTitle }}</a></li>
                   @endforeach
                 </ul>
               </div>
             @endforeach
           </div>
+          <div class="row">
+            <p class="copyright-notice">All Site Content TM and 2020 DC Entertainment, unless otherwise <a href="">noted here</a>. All rights reserved. <a href="">Cookies Settings</a>.</p>
+          </div>
         </div>
-        <div class="col-6 position-relative">
+        <div class="col-6 position-relative d-flex justify-content-end">
           <img src="../../img/dc-logo-bg.png" alt="" class="footer-bg-logo position-absolute">
         </div>
       </div>
     </div>
   </div>
-  <div class="bottom-footer"></div>
+  <div class="bottom-footer">
+    <div class="container h-100">
+      <div class="row h-100">
+        <div class="col-8 d-flex justify-content-start align-items-center">
+          <button type="button" class="btn btn-primary text-uppercase fw-bold px-2 rounded-0 py-2">Sign-up now!</button>
+        </div>
+        <div class="col-4 d-flex justify-content-start align-items-center">
+          <h5 class="fw-bold mb-0">FOLLOW US</h5>
+          <div class="d-flex justify-content-start align-items-center h-100 ms-2">
+            @foreach ($socialIcons as $singleIcon)
+            <div class="ms-4">
+              <a href=""><img src="{{ $singleIcon }}" alt=""></a>
+            </div>
+            @endforeach
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </footer>
